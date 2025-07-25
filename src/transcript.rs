204cbefaf747c9645bf050c5f6a2c1d5f1d9376a
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-07-25 23:13:43 +0300

    wnla from distributed-lab/bp-pp
    https://github.com/distributed-lab/bp-pp

---
+++ src/transcript.rs
@@ -0,0 +1,14 @@
+use k256::elliptic_curve::group::GroupEncoding;
+use k256::elliptic_curve::PrimeField;
+use k256::{FieldBytes, ProjectivePoint, Scalar};
+use merlin::Transcript;
+
+pub fn app_point(label: &'static [u8], p: &ProjectivePoint, t: &mut Transcript) {
+    t.append_message(label, p.to_bytes().as_slice());
+}
+
+pub fn get_challenge(label: &'static [u8], t: &mut Transcript) -> Scalar {
+    let mut buf = [0u8; 32];
+    t.challenge_bytes(label, &mut buf);
+    Scalar::from_repr(*FieldBytes::from_slice(&buf)).unwrap()
+}
