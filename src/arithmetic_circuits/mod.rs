3e762c71b56d913ee248f4b18aa9ec7fe1d856c6
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-07-27 20:14:30 +0300

    arithmetic circuit common inputs type

--- src/arithmetic_circuits/mod.rs
+++ src/arithmetic_circuits/mod.rs
@@ -1,3 +1,14 @@
 mod commit;
 mod inner_circuit;
 mod utils;
+
+use ark_ec::CurveGroup;
+pub use utils::*;
+
+/// Impl ArithmeticCircuit for CurveGroup with BaseField
+impl<G> ArithmeticCircuit<G::BaseField, G>
+where
+    G: CurveGroup,
+{
+    // todo
+}
--- src/arithmetic_circuits/utils.rs
+++ src/arithmetic_circuits/utils.rs
@@ -19,4 +19,23 @@ pub enum WitnessLocation {
     NO(usize),
 }
 
+// Witness layout mapping function type
+// maps wo index to its location in lo/lr/ll/no
 pub type LayoutMapFn = HashMap<usize, WitnessLocation>;
+
+type Matrix<T> = Vec<Vec<T>>;
+
+/// Common inputs for arithmetic circuit protocols
+#[derive(Debug, Clone)]
+pub struct ArithmeticCircuit<G, F> {
+    pub g: G,                  // G ∈ G
+    pub g_vec: Vec<G>,         // G ∈ G^Nm
+    pub h_vec: Vec<G>,         // H ∈ G^(Nv+7)
+    pub wm: Matrix<F>,         // Wm ∈ F^(Nm×Nw)
+    pub am: Vec<F>,            // am ∈ F^Nm
+    pub wl: Matrix<F>,         // Wl ∈ F^(Nl×Nw)
+    pub al: Vec<F>,            // al ∈ F^Nl
+    pub fl: bool,              // fl ∈ {0,1}
+    pub fm: bool,              // fm ∈ {0,1}
+    pub v_commitments: Vec<G>, // V ∈ G^k
+}
